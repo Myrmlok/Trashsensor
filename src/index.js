3bfe5739d6ba8e854480fd6d7c977cb792c4a4f4
Myrmlok <komarovdima753@mail.ru>
2024-09-22 23:13:33 +0300

    update package and remove not working url

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "https://cdn.jsdelivr.net/npm/react-bootstrap@next/dist/react-bootstrap.min.js"
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
